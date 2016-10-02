29b9b53be30392dd4bf20779ca5ce5f3ccbc2366
JSDevL <arfath.7920@gmail.com>
2016-10-02 18:54:20 +0530

    basic template

--- server.js
+++ server.js
@@ -1,56 +1,86 @@
+// for our express server
 var express = require("express");
 var app = express();
+// for all the middlewares we'll use
+/*  logger
+    bodyParser  */
 var middlewares = require("./middlewares.js");
-var PORT = process.env.PORT || 3000;
+// for the persistence of our node objects
+var persist = require('node-persist');
+// for encrytion of database
+var crypto = require('crypto-js');
 
-var db = [
-    {
-        id: 1,
-        description : "Hi"
-    },
-    {
-        id: 2,
-        description : "Hello"
-    }
-]
+var PORT = process.env.PORT || 3000;
 
 app.use(middlewares.logger);
 app.use(middlewares.bodyParserJson);
-app.use(express.static(__dirname + '/public'));
 
-/*******************************************
+persist.initSync();
 
-GET methods
+var initObject = function(){
+    this.index = -1;
+};
 
-*******************************************/
-
-app.get("/api", function(req, res){
-    res.json(db);
-});
-
-app.get("/api/:id", function(req, res){
-
-    db.forEach(function(item){
-        if( parseInt(req.params.id) === item.id )
+var initDatabases = function(){
+    for (var i = 0; i < arguments.length; i++) {
+        if( typeof arguments[i] === "string" && persist.getItemSync(arguments[i]) === undefined )
         {
-            res.json(item);
+            persist.setItemSync(arguments[i], JSON.stringify(new initObject()));
         }
-    });
+    }
+}
 
-    res.status(404).send();
-});
+//initDatabases('Accounts', 'Announcements');
 
-/*******************************************
 
-POST methods
 
-*******************************************/
 
-app.post("/api", function(req, res){
-    console.log(req.body);
-    res.json( req.body );
-});
 
-app.listen(PORT, function(){
-    console.log("server started");
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /*******************************************
+//
+// GET methods
+//
+// *******************************************/
+//
+// app.get("/api", function(req, res){
+//     res.json(db);
+// });
+//
+// /*******************************************
+//
+// POST methods
+//
+// *******************************************/
+//
+// app.post("/api", function(req, res){
+//     console.log(req.body);
+//     res.json( req.body );
+// });
+//
+// //  for static files
+// app.use(express.static(__dirname + '/public'));
+//
+// app.listen(PORT, function(){
+//     console.log("server started");
+// });
